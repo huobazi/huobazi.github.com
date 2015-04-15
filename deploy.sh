f576ce9ac0894dab3f07d8f9e45d8f7b28d4b076
Marble Wu <huobazi@gmail.com>
2015-04-16 00:02:54 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -18,21 +18,20 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin hugo
 
-git subtree push --prefix=public origin master
 # Go To Public folder
-#cd public
+cd public
 # Add changes to git.
-#git add -A
+git add -A
 
 # Commit changes.
-#msg="Site updated at `date`"
-#if [ $# -eq 1 ]
-  #then msg="$1"
-#fi
-#git commit -m "$msg"
+msg="Site updated at `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 
 # Push source and build repos.
-#git push origin master
+git push origin master
 
 # Come Back
-#cd ..
+cd ..
