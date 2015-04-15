ac148023fe04e2f63f9cf46b0f68a3406eee47e9
Marble Wu <huobazi@gmail.com>
2015-04-15 23:30:21 +0800

    Site updated at Wed Apr 15 23:30:21 CST 2015

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ hugo -t flatten
 git add -A
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="Site updated at `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
@@ -18,21 +18,4 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin hugo
 
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add -A
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back
-cd ..
+git subtree push --prefix=public origin master
