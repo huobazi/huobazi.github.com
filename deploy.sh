5781ad5cdc0071d8d3bb99d8eb3165895d63c6ee
Marble Wu <huobazi@gmail.com>
2015-04-16 00:52:03 +0800

    Site source updated at Thu Apr 16 00:52:03 CST 2015

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,24 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin hugo
 
+echo -e "\033[0;32mDeploying themes  to GitHub...\033[0m"
+
+cd themes/purehugo/
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="Site source updated at `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+cd ../../
 
 echo -e "\033[0;32mDeploying site to GitHub...\033[0m"
 
